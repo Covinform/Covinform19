637ab93fee591c8d5459db489a3280ae9dcc27f7
Covinform19 <83092516+Covinform@users.noreply.github.com>
2021-04-23 14:40:56 -0500

    Add files via upload

---
+++ js/main2.js
@@ -0,0 +1,13 @@
+        function videoslider(links){
+            document.querySelector(".slider").src = links;
+        }
+
+        function videoslider2(links){
+            document.querySelector(".slider2").src = links;
+        }
+
+        
+        function videoslider3(links){
+            document.querySelector(".slider3").src = links;
+        }
+
